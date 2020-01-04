b5ffcf027830db8b266bd953908ab9c7eefddb41
dueka <ekanemd@live.com>
2020-01-04 14:30:23 +0100

    Created the profile route

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
+import Profile from "./components/Profile";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
+        <PrivateRoute exact path="/profile" component={Profile} />
         <Route exact path="/" component={Register} />
         <Route exact path="/login" component={Login} />
       </header>
