6a49faf8a95d845cc3efbaf2c1dfbba2e0aef421
Luis Schekerka <luis@Luiss-MBP.fritz.box>
2020-01-07 15:19:44 +0100

    added the users items list to the profile of every other user and added some new styling

--- client/src/components/OtherUsersProfile.js
+++ client/src/components/OtherUsersProfile.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axiosWithAuth from "./axiosWithAuth";
+import styled from "styled-components";
 
 export default function OtherUsersProfile(props) {
   const [users, setUsers] = useState({});
+  const [usersItems, setUsersItems] = useState([]);
 
   useEffect(() => {
     axiosWithAuth()
@@ -17,10 +19,40 @@ export default function OtherUsersProfile(props) {
       });
   });
 
+  axiosWithAuth()
+    .get(
+      `https://lbs-african-marketplace.herokuapp.com/users/${props.userId}/items`
+    )
+    .then(response => {
+      setUsersItems(response.data.items);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
   return (
     <div>
       <h1>Hello, my name is {users.username}</h1>
       <h3>I'm a {users.department} on this marketplace</h3>
+      <h3>This is my Itemlist:</h3>
+      {usersItems.map((item, index) => {
+        return (
+          <ItemCard key={index}>
+            <p>Name: {item.name}</p>
+            <p>Description: {item.description}</p>
+            <p>Price: {item.price}</p>
+          </ItemCard>
+        );
+      })}
     </div>
   );
 }
+
+// Styling here:
+
+const ItemCard = styled.div`
+  background-color: lightblue;
+  color: black;
+  padding: 1%;
+  margin: 2%;
+`
--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -26,7 +26,7 @@ const ProfileCard = props => {
       <UsersItemsList />
       <AddItem />
       <Link to="/login">
-        <button onClick={() => handleSubmit()}>Delete your profile</button>
+        <Button onClick={() => handleSubmit()}>Delete your profile</Button>
       </Link>
     </Card>
   );
@@ -40,6 +40,28 @@ const Card = styled.div`
   align-items: center;
   margin: 1vw;
   padding: 1vw;
-  background: lightblue;
+  background: white;
   color: orange;
+  border-radius: 5px;
+  transition: 0.2s ease-in-out
+  width: 333px;
+  margin: 30px;
+  &:hover {
+    box-shadow: 0px 5px 37px -22px  rgba(0, 0, 0, 1);
+  }
 `;
+
+const Button = styled.button`
+  border-radius: 5px;
+  border: 1px solid blue;
+  padding: 10px;
+  background: transparent;
+  transistion: 0.15s ease-in-out;
+  margin: 10px 0;
+  color: blue;
+  margin-left: 25px;
+  &:hover {
+    cursor: pointer;
+    background: black;
+    color: red;
+  }`
